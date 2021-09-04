47e3f5b21c6f63dff8ed9b8ce5c78fc41dc0a30a
Kacper <kziejlo@gmail.com>
2021-09-04 16:26:58 +0200

    refactor: added better phone appearance

--- src/Components/Forum/ForumMenu.tsx
+++ src/Components/Forum/ForumMenu.tsx
@@ -5,11 +5,12 @@ import { useHistory } from "react-router-dom";
 import { AdditionalForumCategories } from "../../data/Forum/AdditionalForumCategories";
 import { ForumCategory } from "../../data/Forum/ForumCategory";
 
-import "./css/ForumMenu.css"
-
 const useStyles = makeStyles((theme) => ({
     subListHeader: {
         textAlign: 'left',
+        '@media (max-width: 960px)': {
+            textAlign: 'center',
+        },
     },
 }));
 
@@ -23,7 +24,7 @@ export default function ForumMenu(props: ForumMenuprops) {
     const subList = { fontSize: '1.5rem' }
     const checkboxLabel = { fontSize: '0.8rem' }
     const checkbox = { fontSize: '0.8rem', color: "rgb(34, 206, 43)", '&.Mui-checked': { color: "rgb(34, 206, 43)" } }
-    const listItem = { paddingLeft: '3%', margin: "1%", '&:hover': { cursor: 'pointer', backgroundColor: "rgb(242, 242, 242)", borderRadius: "5%" } }
+    const listItem = { paddingLeft: '3%', margin: "1%", '@media (max-width: 960px)': { textAlign: 'center' }, '&:hover': { cursor: 'pointer', backgroundColor: "rgb(242, 242, 242)", borderRadius: "5%" } }
     const listItemMainText = { fontSize: '1.3rem' }
 
     const [checked, setChecked] = useState<boolean>(false);
