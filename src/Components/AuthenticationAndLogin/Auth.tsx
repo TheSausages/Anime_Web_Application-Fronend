abb6610a3f59bb2110b3b95d2d51544f000f310d
Kacper <kziejlo@gmail.com>
2021-10-08 21:14:31 +0200

    refactor: corrected Auth.tsx structure

--- src/Components/AuthenticationAndLogin/Auth.tsx
+++ src/Components/AuthenticationAndLogin/Auth.tsx
@@ -23,105 +23,100 @@ interface ProvideAuthProps {
 }
 
 export function ProvideAuth(props: ProvideAuthProps) {
-  const auth = useProvideAuth();
-  return <authContext.Provider value={auth}>{props.children}</authContext.Provider>;
+    const auth = useProvideAuth();
+    return <authContext.Provider value={auth}>{props.children}</authContext.Provider>;
 }
 
 export const useAuth = () => {
-  return useContext(authContext);
+    return useContext(authContext);
 };
 
 function useProvideAuth(): AuthReturn {
-  let history = useHistory();
-  const { snackbar } = useBasicState()
-  const [rerender, setRerender] = useState<boolean>(false);
+    let history = useHistory();
+    const { snackbar } = useBasicState()
+    const [rerender, setRerender] = useState<boolean>(false);
   
-  const signin = async (cred: Credentials) => {
-    await UserService.login(cred)
-    .then(data => {  
-      localStorage.setItem('accessToken', data.access_token);
-      localStorage.setItem('refreshToken', data.refresh_token);
-      localStorage.setItem('username', cred.username);
-      localStorage.setItem('refreshIfLaterThen', new Date(new Date().getTime() + data.expires_in*800).toISOString())
-      setRerender(!rerender);
+    const signin = async (cred: Credentials) => {
+        await UserService.login(cred)
+        .then(data => {  
+            localStorage.setItem('accessToken', data.access_token);
+            localStorage.setItem('refreshToken', data.refresh_token);
+            localStorage.setItem('username', cred.username);
+            localStorage.setItem('refreshIfLaterThen', new Date(new Date().getTime() + data.expires_in*800).toISOString())
+            setRerender(!rerender);
 
-      history.goBack()
-      snackbar("Logged In Successfully",  snackBarSuccess )
-    })
-    .catch((error: BackendError) => {
-      snackbar(error.message,  snackbarError )
-    })
-  };
+            history.goBack()
+            snackbar("Logged In Successfully",  snackBarSuccess )
+        })
+        .catch((error: BackendError) => snackbar(error.message,  snackbarError))
+    };
 
-  const signout = async () => {
-    await UserService.logout()
-    .then(data => {
-      clearTokenFields();
-      setRerender(!rerender);
+    const signout = async () => {
+        await UserService.logout()
+        .then(data => {
+            clearTokenFields();
+            setRerender(!rerender);
 
-      history.goBack()
-      snackbar("Logged Out Successfully",  snackBarSuccess )
-    })
-    .catch((error: BackendError) => {
-      snackbar(error.message,  snackbarError )
-    })
-  };
+            history.goBack()
+            snackbar("Logged Out Successfully",  snackBarSuccess )
+        })
+        .catch((error: BackendError) => {
+            snackbar(error.message,  snackbarError )
+        })
+    };
 
-  const register = async (regis: RegistrationBody) => {
-    await UserService.register(regis)
-    .then(data => {  
-      localStorage.setItem('accessToken', data.access_token);
-      localStorage.setItem('refreshToken', data.refresh_token);
-      localStorage.setItem('username', regis.username);
-      localStorage.setItem('refreshIfLaterThen', new Date(new Date().getTime() + data.expires_in*1000).toISOString())
-      setRerender(!rerender);
+    const register = async (regis: RegistrationBody) => {
+        await UserService.register(regis)
+        .then(data => {  
+            localStorage.setItem('accessToken', data.access_token);
+            localStorage.setItem('refreshToken', data.refresh_token);
+            localStorage.setItem('username', regis.username);
+            localStorage.setItem('refreshIfLaterThen', new Date(new Date().getTime() + data.expires_in*1000).toISOString())
+            setRerender(!rerender);
 
-      history.goBack();
-      history.goBack();
-      snackbar("Registered Successfully",  snackBarSuccess )
-    })
-    .catch((error: BackendError) => {
-      snackbar(error.message,  snackbarError )
-    })
-  };
+            history.goBack();
+            history.goBack();
+            snackbar("Registered Successfully",  snackBarSuccess)
+        })
+        .catch((error: BackendError) => snackbar(error.message,  snackbarError))
+    };
 
-  const rerenderThisComponent = () => {
-    return rerender
-  }
+    const rerenderThisComponent = () => {
+        return rerender
+    }
 
-  return {
-    rerenderThisComponent,
-    signin,
-    register,
-    signout,
-  };
+    return {
+        rerenderThisComponent,
+        signin,
+        register,
+        signout,
+    };
 }
 
 interface PrivateRouteProps {
-  path: string,
-  children: React.ReactNode
+    path: string,
+    children: React.ReactNode
 }
 
 export function clearTokenFields() {
-  localStorage.removeItem('accessToken')
-  localStorage.removeItem('refreshToken')
-  localStorage.removeItem('refreshIfLaterThen')
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    localStorage.removeItem('refreshIfLaterThen')
 }
 
 export function PrivateRoute(props: PrivateRouteProps) {  
-  return (
-      <Route
-        render={({location}) =>
-          (checkIfLoggedIn() ? (
-            props.children
-          ) : (
-            <Redirect push to={{
-              pathname: '/login',
-              state: { from: location }
-            }}
-            />
-          ))
-        }
-      />
+    return (
+        <Route
+            render={({location}) =>
+                (checkIfLoggedIn() ? (
+                    props.children
+                ) : (
+                    <Redirect push to={{
+                        pathname: '/login',
+                        state: { from: location }
+                    }} />
+                ))
+            }
+        />
     );
-  }
+}
